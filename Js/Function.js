40f266d6047466f6044a52d4d7ca079632582dcc
Madrynense <madrynense@gmail.com>
2023-01-28 15:28:11 -0300

    Tercer commit

--- Js/Function.js
+++ Js/Function.js
@@ -1,3 +1,24 @@
 function myFunction() {
     document.getElementById("demo").innerHTML = "Soy muy buena resolviendo acertijos";
   }
+  
+
+ document.getElementById("cambiar_color").addEventListener("click", function () {
+  document.body.style.backgroundColor = 'green';
+ });
+
+ document.getElementById("cambiar_color2").addEventListener("click", function () {
+  document.body.style.backgroundColor = '#F4BFF3';
+ });
+
+ document.getElementById("cambiar_color3").addEventListener("click", function () {
+  document.body.style.backgroundColor = '#FAD1C4';
+ });
+
+ document.getElementById("cambiar_color4").addEventListener("click", function () {
+  document.body.style.backgroundColor = '#efef83';
+ });
+
+ document.getElementById("ocultar_pasatiempo").addEventListener("click", function() {
+  document.getElementById("demo").style.display = "none";
+ });
